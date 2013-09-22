a023a07f36a29cfb7c4c667e7de290acfab0adcb
Lluis Ramon <lluisramon.tgn@gmail.com>
2013-09-22 14:34:03 +0200

    Delete unused roxygen tags.

--- R/remoteWebElement-class.R
+++ R/remoteWebElement-class.R
@@ -1,21 +1,3 @@
-#' remote Web Element class
-#'
-#' Some additional details about this S4 generic and its methods.
-#' The extra blank line between this section and the title is.
-#' 
-#' @param ... Additional argument list that might not ever used.
-#'
-#' @return A remoteWebElementClass
-#' 
-#' @aliases initialize remoteWebElementClass-method
-#' 
-#' @author Aleix Ruiz de Villa, Andreu Vall, Llu?s Ramon
-#' @seealso \code{\link{firefox}}
-#' 
-#' @export
-#' 
-
-
 remoteWebElementClass <- setRefClass("remoteWebElementClass", fields = list(javaKeys = "javaKeysClass", 
                                                                             javaWebElement = "ANY",
                                                                             javaSelect = "ANY",
